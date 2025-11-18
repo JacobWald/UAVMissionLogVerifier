fcf0d8c92bf5e2b8ba2e1a336a0dd609ffc90e0f
Jacob-Wald <78454759+Jacob-Wald@users.noreply.github.com>
2025-11-18 00:33:57 -0500

    Adding Kevin's work w/ Ethereum

--- uav-ledger/uavledger/urls.py
+++ uav-ledger/uavledger/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from storage.views import home, flights_page, flight_versions_page
+from ledger.views import chain_info_view
 
 urlpatterns = [
     path("", home, name="home"),
     path("flights/", flights_page, name="flights_page"),
     path("flights/<str:flight_id>/", flight_versions_page, name="flight_versions_page"),
+    path("api/chain-info/", chain_info_view, name="chain-info"),
     path("admin/", admin.site.urls),
 ]
